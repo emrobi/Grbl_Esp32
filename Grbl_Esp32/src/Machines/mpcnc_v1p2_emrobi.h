2676eaad96ff030bcba9904ad67f007cdfd044f9
Eric <emrobi@gmail.com>
2020-12-13 20:33:51 -0800

    Fixed build name

--- Grbl_Esp32/src/Machines/mpcnc_v1p2_emrobi.h
+++ Grbl_Esp32/src/Machines/mpcnc_v1p2_emrobi.h
@@ -27,7 +27,7 @@
 
 // // Pin assignments for the Buildlog.net MPCNC controller
 
-#define MACHINE_NAME "MPCNC_V1P2"
+#define MACHINE_NAME "MPCNC_V1P2_EMROBI_20201213"
 
 #define DEFAULT_HOMING_SQUARED_AXES (bit(X_AXIS) | bit(Y_AXIS))
 
